f4113b746b6c56f375f0299bcfb0bfbafd73abce
KEVIN VAN SCHELLEBECK <kevin.vanschellebeck@gmail.com>
2024-01-30 15:17:15 +0100

    complete create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -19,6 +19,7 @@ import {
   signOutUserSuccess,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
   const fileRef = useRef(null);
@@ -185,6 +186,12 @@ const Profile = () => {
         >
           {loading ? "Loading..." : "update"}
         </button>
+        <Link
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+          to={"/create-listing"}
+        >
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
