6006bd03a5a878f1f08d806e62afb103a36fd940
KEVIN VAN SCHELLEBECK <kevin.vanschellebeck@gmail.com>
2024-02-04 17:53:45 +0100

    complete update listing functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -3,6 +3,7 @@ import {
   createListing,
   deleteListing,
   updateListing,
+  getListing,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/create", verifyToken, createListing);
 router.post("/update/:id", verifyToken, updateListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
+router.get("/get/:id", getListing);
 
 export default router;
